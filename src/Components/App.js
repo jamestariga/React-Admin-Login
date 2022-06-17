32d4e977566661906bc9ed6d62eec8239f1ba280
James <jamestariga08@live.com>
2022-06-16 20:45:59 -0700

    ✨ Added routes

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import Register from './Register/Register'
+import Login from './Login/Login'
 import GlobalStyle from './GlobalStyles'
+import { Routes, Route } from 'react-router-dom'
 
 const App = () => {
   return (
     <>
       <GlobalStyle />
-      <Register />
+      <Routes>
+        <Route path='/' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
     </>
   )
 }
