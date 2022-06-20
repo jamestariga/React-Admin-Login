a8eea6d55e4aae3964322a13d43b77d234b25738
James <jamestariga08@live.com>
2022-06-19 17:59:43 -0700

    ✨ Completed global style for all of the pages

--- src/Components/Pages/Styles/Pages.styles.js
+++ src/Components/Pages/Styles/Pages.styles.js
@@ -1,10 +1,69 @@
 import styled from 'styled-components'
 
-export const Container = styled.div`
+export const LayoutContainer = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
+`
+
+export const Wrapper = styled.div`
+  display: flex;
   align-items: center;
-  min-height: 100vh;
-  padding: 1rem 0.5rem;
+  justify-content: center;
+  height: 100vh;
+  width: 100vw;
+`
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 80vh;
+  width: 50%;
+  background-color: firebrick;
+  padding: 2rem;
+  box-shadow: rgba(0, 0, 0, 0.5) 0px 5px 15px;
+  border-radius: 2rem;
+
+  div {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  h1 {
+    font-size: 2rem;
+    font-weight: bold;
+    color: #333;
+  }
+
+  a {
+    margin-top: 1rem;
+    text-decoration: none;
+    color: inherit;
+    margin: 0 -0.25rem;
+    padding: 0 0.35rem;
+    transition: color 0.5s ease-in-out, box-shadow 0.5s ease-in-out;
+    border-radius: 0.5rem;
+
+    &:hover {
+      box-shadow: inset 130px 0 0 0 #54b3d6;
+      color: white;
+    }
+  }
+
+  p {
+    padding: 1rem;
+    font-size: 1.5rem;
+    color: #333;
+  }
+
+  @media (max-width: 768px) {
+    width: 60%;
+
+    p {
+      font-size: 1.1rem;
+      text-align: center;
+    }
+  }
 `
