0a981cb52772756d902d0c3e02b1f1e89c16a20c
James <jamestariga08@live.com>
2022-06-23 21:19:17 -0700

    ✨ Implemented the routes

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import Register from './Register/Register'
 import Login from './Login/Login'
+import Layout from '../Pages/Layout'
+import Home from '../Pages/Home'
+import Public from '../Pages/Public'
+import Admin from '../Pages/Admin'
 import GlobalStyle from './GlobalStyles'
 import { Routes, Route } from 'react-router-dom'
 
@@ -9,8 +13,13 @@ const App = () => {
     <>
       <GlobalStyle />
       <Routes>
-        <Route path='/' element={<Register />} />
-        <Route path='/login' element={<Login />} />
+        <Route path='/' element={<Layout />}>
+          <Route path='/' element={<Public />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/login' element={<Login />} />
+        </Route>
+
+        <Route path='/home' element={<Home />} />
       </Routes>
     </>
   )
--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './Components/App'
+// import GlobalStyle from '../src/Components/GlobalStyles'
 import { AuthProvider } from './context/AuthProvider'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <Router>
+      <AuthProvider>
+        <Routes>
+          {/* <GlobalStyle /> */}
+          <Route path='/*' element={<App />} />
+        </Routes>
+      </AuthProvider>
+    </Router>
   </React.StrictMode>
 )
