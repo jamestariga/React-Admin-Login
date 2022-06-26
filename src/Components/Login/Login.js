0f7fc23481c9df6f53c551042af67be3060f0a1e
James <jamestariga08@live.com>
2022-06-25 18:08:03 -0700

    ✨ Implemented react router to navigate the user to the user home page

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,5 +1,6 @@
-import { useState, useEffect, useRef, useContext } from 'react'
-import AuthContext from '../../context/AuthProvider'
+import { useState, useEffect, useRef } from 'react'
+import useAuth from '../../Hooks/useAuth'
+import { Link, useNavigate, useLocation } from 'react-router-dom'
 import axios from '../../api/axios'
 import LoginUser from './LoginUser'
 import LoginPassword from './LoginPassword'
@@ -11,7 +12,6 @@ import {
   FormGroup,
   FormContainer,
 } from './Styles/Login.styles'
-import { Link } from 'react-router-dom'
 
 const LOGIN_URL = '/auth'
 
@@ -20,12 +20,14 @@ const Login = () => {
   const [password, setPassword] = useState('')
   const [errMsg, setErrMsg] = useState('')
   const [error, setError] = useState(false)
-  const [success, setSuccess] = useState(false)
 
   const userRef = useRef()
   const errRef = useRef()
 
-  const { setAuth } = useContext(AuthContext)
+  const { setAuth } = useAuth()
+  const navigate = useNavigate()
+  const location = useLocation()
+  const from = location.state?.from?.pathname || '/home'
 
   useEffect(() => {
     userRef.current.focus()
@@ -53,7 +55,7 @@ const Login = () => {
       setAuth({ user, password, roles, accessToken })
       setUser('')
       setPassword('')
-      setSuccess(true)
+      navigate(from, { replace: true })
     } catch (err) {
       if (!err?.response) {
         setErrMsg('No Server Response')
@@ -72,56 +74,36 @@ const Login = () => {
     }
   }
 
-  // const handleSubmit = async (e) => {
-  //   e.preventDefault()
-  //   console.log(user, password)
-  //   setUser('')
-  //   setPassword('')
-  //   setSuccess(true)
-  // }
-
   return (
     <>
       <Backdrop>
         <LoginContainer>
           <SideContainer />
           <FormContainer>
-            {success ? (
-              <>
-                <h1>Login Successful!</h1>
-                {/* <button onSubmit={handleLogout}>Logout</button> */}
-              </>
-            ) : (
-              <>
-                <h1>Welcome!</h1>
-                <LoginError errMsg={errMsg} error={error} errRef={errRef} />
-                <form onSubmit={handleSubmit}>
-                  <FormGroup>
-                    <label htmlFor='username'>Username:</label>
-                    <LoginUser
-                      user={user}
-                      setUser={setUser}
-                      userRef={userRef}
-                    />
-                  </FormGroup>
-                  <FormGroup>
-                    <label htmlFor='password'>Password:</label>
-                    <LoginPassword
-                      password={password}
-                      setPassword={setPassword}
-                    />
-                  </FormGroup>
-                  <button>Submit</button>
-                </form>
-                <p>
-                  Need an account?
-                  {/* Change this to a router link */}
-                  <span>
-                    <Link to='/'> Sign up</Link>
-                  </span>
-                </p>
-              </>
-            )}
+            <>
+              <h1>Welcome!</h1>
+              <LoginError errMsg={errMsg} error={error} errRef={errRef} />
+              <form onSubmit={handleSubmit}>
+                <FormGroup>
+                  <label htmlFor='username'>Username:</label>
+                  <LoginUser user={user} setUser={setUser} userRef={userRef} />
+                </FormGroup>
+                <FormGroup>
+                  <label htmlFor='password'>Password:</label>
+                  <LoginPassword
+                    password={password}
+                    setPassword={setPassword}
+                  />
+                </FormGroup>
+                <button>Submit</button>
+              </form>
+              <p>
+                Need an account?
+                <span>
+                  <Link to='/'> Sign up</Link>
+                </span>
+              </p>
+            </>
           </FormContainer>
         </LoginContainer>
       </Backdrop>
