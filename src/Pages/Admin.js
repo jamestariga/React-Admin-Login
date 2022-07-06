9497bf187bb299340457ef2b4adee34a5a2c2250
James <jamestariga08@live.com>
2022-07-05 20:04:51 -0700

    ✨ Minor update

--- src/Pages/Admin.js
+++ src/Pages/Admin.js
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom'
-import { Wrapper, Container } from './Styles/Pages.styles'
+import { Wrapper, Container } from './Styles/Admin.styles'
 import Users from '../Components/User/Users'
 
 const Admin = () => {
@@ -8,7 +8,6 @@ const Admin = () => {
       <Wrapper>
         <Container>
           <h1>Admin Page</h1>
-          <p>Admin can chill here!</p>
           <Users />
           <p>
             <Link to='/home'>Go Home</Link>
