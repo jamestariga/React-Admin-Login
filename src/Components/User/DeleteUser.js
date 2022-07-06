a18fede9fac99287995fbd458d5d0ea46bd167ab
James <jamestariga08@live.com>
2022-07-05 20:00:13 -0700

    ✨ Added a component that will delete a user

---
+++ src/Components/User/DeleteUser.js
@@ -0,0 +1,56 @@
+import { Wrapper, Table, Button } from './Styles/Users.styles'
+
+const DeleteUser = (props) => {
+  const { users, setUsers, axiosPrivate, navigate, location } = props
+
+  // Delete a user with the provided user id that will be deleted from the database the body is the user id
+  const handleDeleteUser = async (userId) => {
+    const controller = new AbortController()
+    try {
+      const response = await axiosPrivate.delete('/employees', {
+        signal: controller.signal,
+        data: { id: userId },
+      })
+      console.log(response.data)
+
+      response.status === 200 &&
+        setUsers(users.filter((user) => user.id !== userId))
+
+      navigate('/home', { state: { from: location }, replace: true })
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  return (
+    <Wrapper>
+      <Table>
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users?.map((user) => (
+            <tr key={user._id}>
+              <td>{user.firstName}</td>
+              <td>{user.lastName}</td>
+              <td>
+                <Button
+                  type='button'
+                  onClick={() => handleDeleteUser(user._id)}
+                >
+                  Delete
+                </Button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </Wrapper>
+  )
+}
+
+export default DeleteUser
