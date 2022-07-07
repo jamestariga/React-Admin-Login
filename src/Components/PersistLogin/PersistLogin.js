6580d7db0c32dc479d82462b25b9be87273abb97
James <jamestariga08@live.com>
2022-07-06 22:23:43 -0700

    ✨ Implemented a persist login to keep the user logged in upon refreshing the tab

---
+++ src/Components/PersistLogin/PersistLogin.js
@@ -0,0 +1,40 @@
+import { Outlet } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import useRefreshToken from '../../Hooks/useRefreshToken'
+import useAuth from '../../Hooks/useAuth'
+
+const PersistLogin = () => {
+  const [isLoading, setIsLoading] = useState(true)
+  const refresh = useRefreshToken()
+  const { auth, persist } = useAuth()
+
+  useEffect(() => {
+    let isMounted = true
+    const verifyRefreshToken = async () => {
+      try {
+        await refresh()
+      } catch (err) {
+        console.error(err)
+      } finally {
+        isMounted && setIsLoading(false)
+      }
+    }
+
+    !auth?.accessToken && persist ? verifyRefreshToken() : setIsLoading(false)
+
+    return () => (isMounted = false)
+  }, [])
+
+  useEffect(() => {
+    console.log(`isLoading: ${isLoading}`)
+    console.log(`aT: ${JSON.stringify(auth?.accessToken)}`)
+  }, [isLoading])
+
+  return (
+    <>
+      {!persist ? <Outlet /> : isLoading ? <div>Loading...</div> : <Outlet />}
+    </>
+  )
+}
+
+export default PersistLogin
