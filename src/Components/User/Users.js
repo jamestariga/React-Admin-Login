f1b5b282979568ca6326ca8077f32cd574b0af07
James <jamestariga08@live.com>
2022-07-07 20:50:26 -0700

    ✨ Minor update

--- src/Components/User/Users.js
+++ src/Components/User/Users.js
@@ -30,7 +30,7 @@ const Users = () => {
         isMounted && setUsers(response.data)
       } catch (err) {
         console.error(err)
-        navigate('/login', { state: { from: location }, replace: true })
+        // navigate('/login', { state: { from: location }, replace: true })
       }
     }
 
